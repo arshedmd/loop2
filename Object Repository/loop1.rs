10497ea1544c0c3ac00d9772e69d9322e33a9a3f
kctech <kctech@kctech-PC>
2017-07-06 15:49:53 +0530

    commit same

---
+++ Object Repository/loop1.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>loop1</name>
+   <tag></tag>
+   <elementGuidId>20c2d300-9e3a-4df9-9375-9edf3e3605bc</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>sadsffd</value>
+   </webElementProperties>
+</WebElementEntity>
