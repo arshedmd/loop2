b196cea703787bf4652b139acccc59a4e535d13c
kctech <kctech@kctech-PC>
2017-07-06 15:28:23 +0530

    vbhnghckmgh

---
+++ Object Repository/gfbfg.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>gfbfg</name>
+   <tag></tag>
+   <elementGuidId>bec2d787-6ebb-4f21-8c37-3b3bc98defc3</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>fnfjfg</value>
+   </webElementProperties>
+</WebElementEntity>
