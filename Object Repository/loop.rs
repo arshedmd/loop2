644378eb3b9be178737c431ae6441fe09158f8b8
kctech <kctech@kctech-PC>
2017-07-06 15:22:54 +0530

    fren

---
+++ Object Repository/loop.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>loop</name>
+   <tag></tag>
+   <elementGuidId>33faa887-7cff-42a6-892e-f9814729c3be</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>title</name>
+      <type>Main</type>
+      <value>thhf</value>
+   </webElementProperties>
+</WebElementEntity>
